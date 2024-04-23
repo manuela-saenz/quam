7f99a44042cd70cf64cabf4121f1122835cd64eb
cristian-run <kristiankamilo1231@gmail.com>
2024-04-23 08:48:29 -0500

    aperiencia y re posicionamiento de de scripts footer

--- footer.php
+++ footer.php
@@ -102,9 +102,6 @@
     </div>
   </div>
 </footer>
-
-</body>
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
@@ -120,4 +117,5 @@
 <?php } ?>
 <?php wp_footer(); ?>
 
+</body>
 </html>
