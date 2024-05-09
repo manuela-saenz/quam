6d6aea5b42d6189e1865d07a9e6f70057e28c3d7
dbem112 <dbem2022@gmail.com>
2024-05-09 16:17:48 -0500

    payu-pay

--- woocommerce/checkout/order-receipt.php
+++ woocommerce/checkout/order-receipt.php
@@ -19,32 +19,60 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 ?>
-<div class="min-vh-100">
+<div class="min-vh-100 ticket-order">
+
+	<div>
+		<img src="<?php bloginfo('template_url') ?>/assets/media/pago-en-linea-payu.svg" alt="">
+	</div>
 
 	<ul class="order_details">
 		<li class="order">
-			<?php esc_html_e('Order number:', 'woocommerce');?>
-			<strong><?php echo esc_html($order->get_order_number()); ?></strong> 
+			<?php /* esc_html_e('Order number:', 'woocommerce'); echo esc_html($order->get_order_number()); */ ?>
+			<!-- <strong><?php /* echo esc_html($order->get_order_number()); */ ?></strong>  -->
 		</li>
 		<li class="date">
-			<?php esc_html_e('Fecha de solicitud de compra: ', 'woocommerce');
-			echo esc_html(wc_format_datetime($order->get_date_created())) ?>
-			<strong><?php echo esc_html(wc_format_datetime($order->get_date_created())); ?></strong> 
+			<?php /*  esc_html_e('Fecha de solicitud de compra: ', 'woocommerce');
+					echo esc_html(wc_format_datetime($order->get_date_created())) */ ?>
+			<!-- <strong><?php /* echo esc_html(wc_format_datetime($order->get_date_created())); */ ?></strong>  -->
 		</li>
 		<li class="total">
-			<?php esc_html_e('Total:', 'woocommerce'); echo wp_kses_post($order->get_formatted_order_total()); ?>
-			<strong><?php echo wp_kses_post($order->get_formatted_order_total()); ?></strong>
+			<?php /*  esc_html_e('Total:', 'woocommerce'); echo wp_kses_post($order->get_formatted_order_total()); */ ?>
+			<!-- <strong><?php /* echo wp_kses_post($order->get_formatted_order_total()); */ ?></strong> -->
 		</li>
-		<?php if ($order->get_payment_method_title()): ?>
-			<li class="method">
-				<?php esc_html_e('Payment method: ', 'woocommerce'); echo wp_kses_post($order->get_payment_method_title()); ?>
-				<strong><?php echo wp_kses_post($order->get_payment_method_title()); ?></strong>
-			</li>
-		<?php endif; ?>
+		<?php /* if ($order->get_payment_method_title()): */ ?>
+		<li class="method">
+			<?php /* esc_html_e('Payment method: ', 'woocommerce'); echo wp_kses_post($order->get_payment_method_title()); */ ?>
+			<!-- <strong><?php /* echo wp_kses_post($order->get_payment_method_title()); */ ?></strong> -->
+		</li>
+		<?php /* endif; */ ?>
 	</ul>
 	<div class="cont-btn-payu">
-		<?php do_action('woocommerce_receipt_' . $order->get_payment_method(), $order->get_id()); ?>
+		<?php
+		// Obtenemos el texto devuelto por la acción woocommerce_receipt_ y lo almacenamos en una variable
+		ob_start();
+		do_action('woocommerce_receipt_' . $order->get_payment_method(), $order->get_id());
+		$texto = ob_get_clean();
+
+		// Buscamos la posición de la frase "Gracias por su pedido" en el texto devuelto
+		$posicion = strpos($texto, "Gracias por su pedido");
+
+		// Si la frase está presente, agregamos un span alrededor de ella
+		if ($posicion !== false) {
+			$texto_modificado = substr_replace($texto, '<span>', $posicion, 0);
+			$posicion_final = $posicion + strlen('<span>');
+			$texto_modificado = substr_replace($texto_modificado, '</span>', $posicion_final + strlen("Gracias por su pedido"), 0);
+			// Agregamos "!¡" al mensaje
+			$texto_modificado = str_replace('Gracias por su pedido', '¡Gracias por su pedido!', $texto_modificado);
+			// Eliminamos la coma después de la frase
+			$texto_modificado = str_replace(',', '', $texto_modificado);
+			echo $texto_modificado;
+		} else {
+			echo $texto; // Si la frase no está presente, imprimimos el texto original
+		}
+		?>
 	</div>
+
+
 
 </div>
 <div class="clear"></div>
