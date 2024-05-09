3a4ba6d53ed3c48ce5778d76d0e3d0cfd72316eb
dbem112 <dbem2022@gmail.com>
2024-05-08 21:03:36 -0500

    chekout-pay

--- woocommerce/checkout/order-receipt.php
+++ woocommerce/checkout/order-receipt.php
@@ -15,32 +15,45 @@
  * @version 3.2.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 ?>
+<div class="ticket-order pt-5 pb-5">
 
-<ul class="order_details">
-	<li class="order">
-		<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
-		<strong><?php echo esc_html( $order->get_order_number() ); ?></strong>
-	</li>
-	<li class="date">
-		<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
-		<strong><?php echo esc_html( wc_format_datetime( $order->get_date_created() ) ); ?></strong>
-	</li>
-	<li class="total">
-		<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
-		<strong><?php echo wp_kses_post( $order->get_formatted_order_total() ); ?></strong>
-	</li>
-	<?php if ( $order->get_payment_method_title() ) : ?>
-	<li class="method">
-		<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
-		<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
-	</li>
-	<?php endif; ?>
-</ul>
+	<div class=" justify-content-center">
+		<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+			class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+			<path
+				d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+		</svg>
+	</div>
 
-<?php do_action( 'woocommerce_receipt_' . $order->get_payment_method(), $order->get_id() ); ?>
+	<ul class="order_details justify-content-center">
+		<li class="order border-0 fs-6 fw-bold text-body-tertiary m.0">
+			<?php esc_html_e('Order number:', 'woocommerce');
+			echo esc_html($order->get_order_number()); ?>
+			<!-- <strong><?php /* echo esc_html($order->get_order_number()); */ ?></strong> -->
+		</li>
+		<li class="date border-0 fs-6 fw-bold text-body-tertiary m-0">
+			<?php esc_html_e('Fecha de solicitud de compra: ', 'woocommerce');
+			echo esc_html(wc_format_datetime($order->get_date_created())) ?>
+			<!-- <strong><?php /* echo esc_html(wc_format_datetime($order->get_date_created())); */ ?></strong> -->
+		</li>
+		<li class="total border-0 fs-6  text-body-tertiary m-0">
+			<?php esc_html_e('Total:', 'woocommerce'); echo wp_kses_post($order->get_formatted_order_total()); ?>
+			<!-- <strong><?php /* echo wp_kses_post($order->get_formatted_order_total()); */ ?></strong> -->
+		</li>
+		<?php if ($order->get_payment_method_title()): ?>
+			<li class="method border-0 fs-6 fw-bold m-0">
+				<?php esc_html_e('Payment method: ', 'woocommerce'); echo wp_kses_post($order->get_payment_method_title()); ?>
+				<!-- <strong><?php /* echo wp_kses_post($order->get_payment_method_title()); */ ?></strong> -->
+			</li>
+		<?php endif; ?>
+	</ul>
+	<div class="cont-btn-payu">
+		<?php do_action('woocommerce_receipt_' . $order->get_payment_method(), $order->get_id()); ?>
+	</div>
 
+</div>
 <div class="clear"></div>
--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -17,74 +17,99 @@
  * @var WC_Order $order
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 
-<div class="woocommerce-order">
+<div class="woocommerce-order pt-5 pb-5">
 
 	<?php
-	if ( $order ) :
-
-		do_action( 'woocommerce_before_thankyou', $order->get_id() );
+	if ($order):
+		do_action('woocommerce_before_thankyou', $order->get_id());
 		?>
 
-		<?php if ( $order->has_status( 'failed' ) ) : ?>
+		<?php if ($order->has_status('failed')): ?>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
+			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed">
+				<?php esc_html_e('Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce'); ?>
+			</p>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
-				<?php if ( is_user_logged_in() ) : ?>
-					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
+			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions fs-6">
+				<a href="<?php echo esc_url($order->get_checkout_payment_url()); ?>"
+					class="button pay"><?php esc_html_e('Pay', 'woocommerce'); ?></a>
+				<?php if (is_user_logged_in()): ?>
+					<a href="<?php echo esc_url(wc_get_page_permalink('myaccount')); ?>"
+						class="button pay"><?php esc_html_e('My account', 'woocommerce'); ?></a>
 				<?php endif; ?>
 			</p>
 
-		<?php else : ?>
+		<?php else: ?>
 
-			<?php wc_get_template( 'checkout/order-received.php', array( 'order' => $order ) ); ?>
+			<div class="ticket-order">
+				<div>
+					<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+						class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+						<path
+							d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+					</svg>
+				</div>
 
-			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+				<?php wc_get_template('checkout/order-received.php', array('order' => $order)); ?>
 
-				<li class="woocommerce-order-overview__order order">
-					<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
+				<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details justify-content-center">
 
-				<li class="woocommerce-order-overview__date date">
-					<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
-					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
+					<li class="woocommerce-order-overview__date date border-0 fs-6 fw-bold text-body-tertiary">
+						<?php esc_html_e('Fecha de solicitud de compra:', 'woocommerce');
+						echo wc_format_datetime($order->get_date_created()); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 
-				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
-					<li class="woocommerce-order-overview__email email">
-						<?php esc_html_e( 'Email:', 'woocommerce' ); ?>
-						<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+						<!-- <strong><?php /* echo wc_format_datetime($order->get_date_created()); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?></strong>-->
 					</li>
-				<?php endif; ?>
 
-				<li class="woocommerce-order-overview__total total">
-					<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
-
-				<?php if ( $order->get_payment_method_title() ) : ?>
-					<li class="woocommerce-order-overview__payment-method method">
-						<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
-						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+					<li class="fs-6 text-body-tertiary">
+						Tu pedido se encuentra en proceso de validación, en breve recibirás un correo con el detalle de tu
+						compra.
 					</li>
-				<?php endif; ?>
 
-			</ul>
+					<?php if (is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email()): ?>
+						<li class="woocommerce-order-overview__email email border-0 fs-6 text-body-tertiary">
+							<?php esc_html_e('Email:', 'woocommerce'); ?>
+							<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+						</li>
+					<?php endif; ?>
 
+					<!--<li class="woocommerce-order-overview__total total border-0">
+					<?php /* esc_html_e('Total:', 'woocommerce'); ?>
+												  <strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?></strong>
+				</li> -->
+
+					<?php /* if ($order->get_payment_method_title()): ?>
+												<li class="woocommerce-order-overview__payment-method method border-0">
+													<?php esc_html_e('Payment method: ', 'woocommerce');
+													echo wp_kses_post($order->get_payment_method_title()); ?>
+													<strong><?php ?></strong>
+												</li>
+											<?php endif; */ ?>
+
+					<li class="woocommerce-order-overview__order order border-0 fs-6 fw-bold">
+						<?php esc_html_e('Order number:', 'woocommerce');
+						echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						<!-- <strong></*?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */?></strong> -->
+					</li>
+
+					<li>
+						<a class="" href="https://www.quam.com.co/web_quam/">Seguir comprando</a>
+					</li>
+				</ul>
+
+			<?php endif; ?>
+			<!-- <div>
+				<? /* php do_action('woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id()); */ ?>
+			</div> -->
+
+			<div>
+				<?php /* do_action('woocommerce_thankyou', $order->get_id()); */ ?>
+			</div>
+		<?php else: ?>
+			<?php wc_get_template('checkout/order-received.php', array('order' => false)); ?>
 		<?php endif; ?>
-
-		<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
-		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
-
-	<?php else : ?>
-
-		<?php wc_get_template( 'checkout/order-received.php', array( 'order' => false ) ); ?>
-
-	<?php endif; ?>
-
+	</div>
 </div>
