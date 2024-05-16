718bd8033027ea253830024b69b82c14f829f2c3
dbem112 <dbem2022@gmail.com>
2024-05-16 12:45:13 -0500

    filter

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,55 +2,51 @@
 $(window).on("scroll load resize", function () {
   var scroll = $(window).scrollTop();
   if (scroll >= 30) {
-    $('header').addClass("sticky-header");
+    $("header").addClass("sticky-header");
   } else {
-    $('header').removeClass("sticky-header");
+    $("header").removeClass("sticky-header");
   }
 });
 
-$('.menu-btn').on('click', function () {
-  $(this).toggleClass('close')
-  $('.mobile-menu').toggleClass('show-menu')
-  $('.info-contact').toggleClass('infoshow-menu')
-})
+$(".menu-btn").on("click", function () {
+  $(this).toggleClass("close");
+  $(".mobile-menu").toggleClass("show-menu");
+  $(".info-contact").toggleClass("infoshow-menu");
+});
 
-$('.mobile-menu a').on('click', function () {
-  $('.mobile-menu').removeClass('show-menu')
-  $('.menu-btn').removeClass('close')
-  $('.info-contact').removeClass('infoshow-menu')
-  header.removeClass('sticky-header');
-  $('body').removeClass('no-scroll');
-})
+$(".mobile-menu a").on("click", function () {
+  $(".mobile-menu").removeClass("show-menu");
+  $(".menu-btn").removeClass("close");
+  $(".info-contact").removeClass("infoshow-menu");
+  header.removeClass("sticky-header");
+  $("body").removeClass("no-scroll");
+});
 
 $("body").on("click", function () {
-  $('.mobile-menu').removeClass('show-menu')
-  $('.info-contact').removeClass('show-menu')
-})
+  $(".mobile-menu").removeClass("show-menu");
+  $(".info-contact").removeClass("show-menu");
+});
 
 $(".menu-btn , .mobile-menu").on("click", function (e) {
-  e.stopPropagation()
+  e.stopPropagation();
 });
 
 $(window).on("load resize", function () {
   if ($(window).width() <= 1200) {
-    $('.mobile-menu').on('click', function () {
-      $('body').toggleClass('no-scroll');
-    })
+    $(".mobile-menu").on("click", function () {
+      $("body").toggleClass("no-scroll");
+    });
   }
-})
-
-
+});
 
 // <!------------------------ modal description single product ------------------------>//
 //  $('.product-actions').on('click', function () {
 //   $('.product-actions').toggleClass('show-bag')
 // })
 
-
 // $(document).ready(function(){
 //   alert($(window).width());
 // })
-
 
 // <--------- cuntificador ---------->
 /**
@@ -58,89 +54,83 @@ $(window).on("load resize", function () {
  */
 
 function initQuantity() {
-
-  $('.quantity.product').on('click', '.plus', function (e) {
-    let $input = $(this).parent().find('input');
+  $(".quantity.product").on("click", ".plus", function (e) {
+    let $input = $(this).parent().find("input");
     let val = parseInt($input.val());
     $input.val(val + 1).change();
   });
 
-  $('.quantity.product').on('click', '.minus',
-    function (e) {
-      let $input = $(this).parent().find('input');
-      var val = parseInt($input.val());
-      if (val > 1) {
-        $input.val(val - 1).change();
-      }
-    });
-
+  $(".quantity.product").on("click", ".minus", function (e) {
+    let $input = $(this).parent().find("input");
+    var val = parseInt($input.val());
+    if (val > 1) {
+      $input.val(val - 1).change();
+    }
+  });
 }
 
-$('.cfvsw-swatches-option, .btn-step-next button').on('click', function(){
-  $(window).scrollTop(0)
-})
+$(".cfvsw-swatches-option, .btn-step-next button").on("click", function () {
+  $(window).scrollTop(0);
+});
 
 function initQuantitySingle() {
-
-  $('.quantity').on('click', '.plus', function (e) {
-    let $input = $(this).prev('input.qtySingle');
+  $(".quantity").on("click", ".plus", function (e) {
+    let $input = $(this).prev("input.qtySingle");
     let val = parseInt($input.val());
     $input.val(val + 1).change();
   });
 
-  $('.quantity').on('click', '.minus',
-    function (e) {
-      let $input = $(this).next('input.qtySingle');
-      var val = parseInt($input.val());
-      if (val > 0) {
-        $input.val(val - 1).change();
-      }
-    });
-
+  $(".quantity").on("click", ".minus", function (e) {
+    let $input = $(this).next("input.qtySingle");
+    var val = parseInt($input.val());
+    if (val > 0) {
+      $input.val(val - 1).change();
+    }
+  });
 }
-
 
 initQuantity();
 
 // -------- quitar productos del  carrito-----------
 
-$('.shopping-bag-offcanvas .select-bag a.remove').click(function (event) {
+$(".shopping-bag-offcanvas .select-bag a.remove").click(function (event) {
   event.preventDefault();
-  $(this).closest('.select-bag').remove(); // Remueve el elemento con la clase select-bag más cercano al enlace clickeado
+  $(this).closest(".select-bag").remove(); // Remueve el elemento con la clase select-bag más cercano al enlace clickeado
 });
 
-
-$(window).on('load resize', function () {
+$(window).on("load resize", function () {
   if ($(window).width() <= 991) {
-    $('.product-actions').addClass('offcanvas offcanvas-bottom')
+    $(".product-actions").addClass("offcanvas offcanvas-bottom");
   } else {
-    $('.product-actions').removeClass('offcanvas offcanvas-bottom')
+    $(".product-actions").removeClass("offcanvas offcanvas-bottom");
   }
-})
+});
 
-$('.open-selector , .add-fav').on('click', (event) => {
+$(".open-selector , .add-fav").on("click", (event) => {
   event.preventDefault();
 });
 
+$(".woocommerce-ordering select").on("change", function () {
+  $("#infoProducts").addClass("loading");
+});
 
-
-$('.woocommerce-ordering select').on('change', function(){
-  $('#infoProducts').addClass('loading');
-})
+$(".woocommerce-ordering-price button").on("click", function () {
+  $("#infoProducts").addClass("loading");
+});
 
 // precios filtro
 function formatCurrency(input) {
   // Eliminar todos los caracteres que no sean dígitos
-  var value = input.value.replace(/\D/g, '');
+  var value = input.value.replace(/\D/g, "");
 
   // Si el valor es vacío, retornar
   if (!value) return;
 
   // Formatear como número con separadores de miles
-  var formattedValue = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  var formattedValue = value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
   // Agregar símbolo de dólar
-  formattedValue = '$' + formattedValue;
+  formattedValue = "$" + formattedValue;
 
   // Asignar el valor formateado de nuevo al campo de entrada
   input.value = formattedValue;
@@ -148,13 +138,13 @@ function formatCurrency(input) {
 
 function clearCurrencyFormat(input) {
   // Eliminar símbolo de dólar y puntos
-  var value = input.value.replace(/[$.]/g, '');
+  var value = input.value.replace(/[$.]/g, "");
 
   // Si el valor es vacío, mostrar el placeholder
   if (!value) {
-      input.value = '';
-      input.placeholder = input.getAttribute('placeholder');
-      return;
+    input.value = "";
+    input.placeholder = input.getAttribute("placeholder");
+    return;
   }
 
   // Asignar el valor sin formato de nuevo al campo de entrada
@@ -173,75 +163,125 @@ function getUrlParams() {
   var params = {};
   var urlParams = new URLSearchParams(window.location.search);
   for (const [key, value] of urlParams) {
-      params[key] = value;
+    params[key] = value;
   }
   return params;
 }
 
-// Función para mostrar los filtros aplicados en el div
+// obtener los filtros aplicados
 function showAppliedFilters() {
   var params = getUrlParams();
-  var appliedFiltersDiv = document.getElementById('appliedFilters');
-  var filtersHtml = '';
+  var appliedFiltersDiv = document.getElementById("appliedFilters");
+  var filtersHtml = "";
 
   // Verificar si hay filtros aplicados
   var hasFilters = false;
   for (var key in params) {
-      if (params[key] !== '') {
-          hasFilters = true;
-          break;
-      }
+    if (params[key] !== "") {
+      hasFilters = true;
+      break;
+    }
   }
 
   // Si hay filtros aplicados, mostrar el div
   if (hasFilters) {
-      appliedFiltersDiv.style.display = 'flex';
+    appliedFiltersDiv.style.display = "flex";
 
-      filtersHtml += '<ul class="d-flex flex-wrap align-items-center">';
-      // Mostrar los filtros aplicados
-      for (var key in params) {
-          if (params[key] !== '') {
-              if (key === 'min_price' || key === 'max_price') {
-                  continue; // Saltar los parámetros de precio
-              }
-              var filterName = key.replace('filter_', ''); // Eliminar el prefijo 'filter_'
-              filtersHtml += '<li><strong>' + filterName + ':</strong> ' + params[key] + '</li>';
-          }
+    filtersHtml += '<div class="d-flex flex-wrap align-items-center">';
+    // Mostrar los filtros aplicados
+    for (var key in params) {
+      if (params[key] !== "") {
+        if (key === "min_price" || key === "max_price") {
+          continue; // Saltar los parámetros de precio
+        }
       }
+    }
 
-      // Unir los valores de min_price y max_price bajo el nombre "Precio"
-      var minPrice = params['min_price'] || '';
-      var maxPrice = params['max_price'] || '';
-      if (minPrice && maxPrice) {
-          filtersHtml += '<li><strong>Precio:</strong> ' + minPrice + ' - ' + maxPrice + '</li>';
-      } else if (minPrice) {
-          filtersHtml += '<li><strong>Precio mínimo:</strong> ' + minPrice + '</li>';
-      } else if (maxPrice) {
-          filtersHtml += '<li><strong>Precio máximo:</strong> ' + maxPrice + '</li>';
-      }
-      
-      filtersHtml += '</ul>';
+    // Unir los valores de min_price y max_price bajo el nombre "Precio"
+    var minPrice = params["min_price"] || "";
+    var maxPrice = params["max_price"] || "";
 
-      // Mostrar el botón para restablecer los filtros
-      filtersHtml += '<button type="button" class="quam-btn" onclick="resetFilters()">Restablecer filtros</button>';
+    filtersHtml += "</div>";
 
-      appliedFiltersDiv.innerHTML = filtersHtml;
+    // Mostrar el botón para restablecer los filtros
+    filtersHtml +=
+      '<button type="button" class="quam-btn" onclick="resetFilters()">Restablecer filtros</button>';
+
+    appliedFiltersDiv.innerHTML = filtersHtml;
   } else {
-      appliedFiltersDiv.style.display = 'none';
+    appliedFiltersDiv.style.display = "none";
+  }
+
+  // Mostrar por defecto el color seleccionado
+  var filterColor = params["filter_color"];
+  if (filterColor) {
+    var inputColor = document.querySelector('select[name="filter_color"]');
+    if (inputColor) {
+      inputColor.value = filterColor;
+    }
+  }
+
+  // Mostrar por defecto la talla seleccionada
+  var filterTalla = params["filter_talla"];
+  if (filterTalla) {
+    var inputTalla = document.querySelector('select[name="filter_talla"]');
+    if (inputTalla) {
+      inputTalla.value = filterTalla;
+    }
+  }
+
+  // Mostrar por defecto el valor mínimo de precio
+  var filterMinPrice = params["min_price"];
+  if (filterMinPrice) {
+    var inputMinPrice = document.querySelector('input[name="min_price"]');
+    if (inputMinPrice) {
+      inputMinPrice.value = filterMinPrice;
+    }
+  }
+
+  // Mostrar por defecto el valor máximo de precio
+  var filterMaxPrice = params["max_price"];
+  if (filterMaxPrice) {
+    var inputMaxPrice = document.querySelector('input[name="max_price"]');
+    if (inputMaxPrice) {
+      inputMaxPrice.value = filterMaxPrice;
+    }
   }
 }
 
 // Función para restablecer los filtros
 function resetFilters() {
   var url = window.location.href;
-  
+
   // Remover los parámetros específicos de la URL
-  url = url.replace(/([&?]filter_color=|&filter_talla=|&min_price=|&max_price=)[^&]+/g, '');
-  
+  url = url.replace(
+    /([&?]filter_color=|&filter_talla=|&min_price=|&max_price=)[^&]+/g,
+    ""
+  );
+
   // Redirigir a la URL sin los parámetros especificados
   window.location.href = url;
-  console.log(url);
 }
 
 // Llamar a la función al cargar la página
 showAppliedFilters();
+
+// validar cambios en el submit filtro
+function handleFormSubmit() {
+  clearCurrencyFormat(document.getElementById("min_price"));
+  clearCurrencyFormat(document.getElementById("max_price"));
+
+  if (document.getElementById("min_price").value === "") {
+    document.getElementById("min_price").value = "25000";
+  }
+
+  if (document.getElementById("max_price").value === "") {
+    document.getElementById("max_price").value = "500000";
+  }
+}
+
+document
+  .getElementById("filterForm")
+  .addEventListener("submit", function (event) {
+    handleFormSubmit();
+  });
--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -36,8 +36,9 @@ $products = get_products_by_category_name($currentCat->name);
         <div class="row">
             <div class="col-md-12">
                 <!-- <h2 class="section-subtitle mb-0"><?= $currentCat->name ?></h2> -->
-                <?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
-                    <h1 class="woocommerce-products-header__title page-title <?= is_search() ? 'section-subtitle-2' : 'section-subtitle' ?>  mb-0">
+                <?php if (apply_filters('woocommerce_show_page_title', true)): ?>
+                    <h1
+                        class="woocommerce-products-header__title page-title <?= is_search() ? 'section-subtitle-2' : 'section-subtitle' ?>  mb-0">
                         <?php woocommerce_page_title(); ?>
                     </h1>
                 <?php endif; ?>
@@ -56,20 +57,22 @@ $products = get_products_by_category_name($currentCat->name);
                     $cate = get_queried_object();
                     $cateSlug = $cate->slug;
                     //Get all products from this category
-                    $products = wc_get_products(array(
-                        'category' => array($cateSlug),
-                        'posts_per_page' => -1
-                    ));
-                    
+                    $products = wc_get_products(
+                        array(
+                            'category' => array($cateSlug),
+                            'posts_per_page' => -1
+                        )
+                    );
+
 
                     //get all prices from this category
                     $all_prices[] = array();
-                   
+
                     foreach ($products as $product) {
                         $all_prices[] = $product->get_price();
                     }
                     // print_r($all_prices);
-
+                    
                     ?>
 
                     <?php
@@ -112,10 +115,8 @@ $products = get_products_by_category_name($currentCat->name);
                      } */ ?>
 
                     <!-- form filter -->
-
-
-                    <form id="filterForm" onsubmit="clearCurrencyFormat(document.getElementById('min_price')); clearCurrencyFormat(document.getElementById('max_price')); if(document.getElementById('min_price').value === '') { document.getElementById('min_price').value = '25000'; }if(document.getElementById('max_price').value === '') { document.getElementById('max_price').value = '500000'; }" action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?>" method="get">
-
+                    <form id="filterForm" class="woocommerce-ordering-price d-flex align-items-center"
+                        action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?>" method="get">
                         <div class="select-box">
                             <select name="filter_color">
                                 <option value="">Selecciona un color</option>
@@ -140,20 +141,22 @@ $products = get_products_by_category_name($currentCat->name);
                             <div class="arrow"></div>
                         </div>
 
-                        <div class="select-box input-box">
+                        <div class="input-box">
                             <label for="min_price">Precio:</label>
-                            <input type="text" name="min_price" id="min_price" oninput="formatCurrency(this)" onblur="updateValue(this)" placeholder="$25.000">
+                            <input type="text" name="min_price" id="min_price" oninput="formatCurrency(this)"
+                                onblur="updateValue(this)" placeholder="$25.000">
                             <label for="max_price">-</label>
-                            <input type="text" name="max_price" id="max_price" oninput="formatCurrency(this)" onblur="updateValue(this)" placeholder="$500.000">
+                            <input type="text" name="max_price" id="max_price" oninput="formatCurrency(this)"
+                                onblur="updateValue(this)" placeholder="$500.000">
                         </div>
 
-                        <button type="submit" class="quam-btn blue">Filtrar</button>
 
+                        <button type="submit" class="quam-btn blue">Filtrar</button>
                         <div id="appliedFilters" class="filtro-activo-contenedor"></div>
+
                     </form>
                     <!-- Campos ocultos para mantener los parámetros de la URL -->
                     <?php /* wc_query_string_form_fields(null, array('size', 'color', 'min_price', 'max_price')); */ ?>
-
                 </div>
                 <div class="products text-center text-lg-end d-flex flex-wrap align-items-center flex-column">
                     <?= woocommerce_result_count() ?>
