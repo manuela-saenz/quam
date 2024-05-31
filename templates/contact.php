12b65ea7f3011a059ac8ba9fffa32eaf286a441e
Jhonatan <pruebajhdx@gmail.com>
2024-05-31 15:11:21 -0500

    contact

--- templates/contact.php
+++ templates/contact.php
@@ -53,7 +53,7 @@ get_header() ?>
                     <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
                     <path d="M3 7l9 6l9 -6" />
                 </svg>
-                <a>info@quamservice.com</a>
+                <a>legiestsas@gmail.com</a>
             </div>
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clock">
