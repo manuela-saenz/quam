b5bda2d76352832e3de3afcd86009e1377a28ab5
cristian-run <kristiankamilo1231@gmail.com>
2024-10-22 16:31:04 -0500

    pagina terminos y condiciones

---
+++ templates/terminos-condiciones.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * Template Name: Terminos y condiciones
+ */
+
+get_header() ?>
+
+<section class="pb-0">
+    <div class="container">
+        <h1 class="text-center section-subtitle mb-0"><?php the_title() ?></h1>
+    </div>
+</section>
+<section class="pb-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="text-white terms-box">
+                    <h5 class="fw-bolder">Bienvenido al sitio quam.com.co</h5>
+                    <p class="mb-0">QUAM provee este sitio para ofrecer
+                        un mejor servicio a sus clientes. Por
+                        favor lee las siguientes normas que
+                        gobiernan tu uso de quam.com.co,
+                        el cual aceptas al acceder a él. Si no
+                        estás de acuerdo, por favor no lo
+                        uses. Nos reservamos el derecho a
+                        actualizar o cambiar los Términos y
+                        Condiciones en cualquier momento
+                        sin previo aviso, es por esto que te
+                        recomendamos leer esta sección
+                        cada vez que vayas a usar este sitio.</p>
+                </div>
+
+            </div>
+            <div class="col-md-8">
+                <div class="ps-2">
+                    <h3 class="section-title-common-content"><span>01</span>RESTRICCIONES DE USO</h3>
+                    <div>
+                        <p><span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis repellat dignissimos magnam similique aperiam ipsum cum assumenda? A totam vitae ut aut iure laudantium id amet modi. Aliquam, quisquam est.</span><span>Quos et consectetur dolorum in odit neque doloremque provident repellendus, consequuntur placeat impedit facilis ad minima dignissimos libero iure odio enim corporis error harum sunt optio incidunt. Beatae, aliquid tenetur?</span></p>
+                        <p><span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium unde molestiae temporibus perspiciatis, earum veniam eos deserunt labore, et veritatis non quisquam? Minima eveniet accusamus itaque voluptatem iusto qui soluta!</span><span>Perspiciatis, illum. Perspiciatis, id sint. Iure, hic ut? Nulla soluta saepe, amet rerum sed, laborum numquam repellat provident temporibus eos corrupti incidunt ipsum necessitatibus et facilis tempore nisi ducimus! Voluptatibus.</span></p>
+                        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat explicabo repudiandae aliquam, voluptas molestiae eos accusamus hic ducimus alias quibusdam facilis ipsum nesciunt nihil odio? Deleniti quae officiis expedita quos.</span><span>Numquam suscipit dolor veritatis sequi corporis similique mollitia quos, odio expedita facilis laudantium excepturi omnis quo temporibus eius laborum quas est obcaecati hic optio impedit? Blanditiis voluptatibus ipsam praesentium sed.</span></p>
+                        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde accusantium iste officia dolorem eaque iure incidunt repudiandae, adipisci quidem, modi perferendis aliquid omnis tempore molestias est deleniti tempora praesentium expedita.</span><span>Similique labore qui molestiae eveniet delectus laboriosam incidunt fugit vel necessitatibus autem iure libero praesentium error quas sapiente, doloribus ut distinctio ad laborum obcaecati accusantium repellat? Dolores doloremque laudantium autem?</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="img-fit mb-4">
+                    <img src="https://img.freepik.com/foto-gratis/hombre-joven-relajado-que-sostiene-tableta_1262-18592.jpg?t=st=1729632625~exp=1729636225~hmac=82f271d989158a3e68af98f56cb5a25d04b6d0f68d2beef9cd04a00ec8ec299a&w=1060" alt="">
+                </div>
+                <div class="img-fit mb-4">
+                    <img src="https://img.freepik.com/foto-gratis/hombre-joven-relajado-que-sostiene-tableta_1262-18592.jpg?t=st=1729632625~exp=1729636225~hmac=82f271d989158a3e68af98f56cb5a25d04b6d0f68d2beef9cd04a00ec8ec299a&w=1060" alt="">
+                </div>
+                <div class="img-fit mb-4">
+                    <img src="https://img.freepik.com/foto-gratis/hombre-joven-relajado-que-sostiene-tableta_1262-18592.jpg?t=st=1729632625~exp=1729636225~hmac=82f271d989158a3e68af98f56cb5a25d04b6d0f68d2beef9cd04a00ec8ec299a&w=1060" alt="">
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="ps-2">
+
+
+                    <div class="mb-4">
+                        <h3 class="section-title-common-content"><span>02</span>Colores</h3>
+                        <p>QUAM ha realizado un esfuerzo para mostrar los colores de las prendas de la forma más cercana a la realidad posible. No obstante ello, el
+                            color de las prendas que aparecen en pantalla puede estar sujeto a variaciones dependiendo de la calidad del monitor de tu computador. En
+                            este sentido QUAM no puede garantizar que los colores que aparezcan en tu monitor se ajusten fielmente a la realidad.</p>
+                    </div>
+                    <div class="mb-4">
+                        <h3 class="section-title-common-content"><span>03</span>TERMINACIÓN DEL SERVICIO</h3>
+                        <p>Los términos y condiciones se aplican si accedes al sitio, estás en el proceso de registro o tienes un proceso de compra. quam.com.co puede
+                            dar por terminado el servicio en cualquier momento sin previa autorización. Las disposiciones relativas a Derechos de autor, marcas,
+                            responsabilidad, limitación de responsabilidad, indemnización y misceláneo, no desaparecen con esta terminación.</p>
+                    </div>
+                    <div class="mb-4">
+                        <h3 class="section-title-common-content"><span>04</span>TERMINACIÓN DEL SERVICIO</h3>
+                        <p>Los términos y condiciones se aplican si accedes al sitio, estás en el proceso de registro o tienes un proceso de compra. quam.com.co puede
+                            dar por terminado el servicio en cualquier momento sin previa autorización. Las disposiciones relativas a Derechos de autor, marcas,
+                            responsabilidad, limitación de responsabilidad, indemnización y misceláneo, no desaparecen con esta terminación</p>
+                    </div>
+                    <div class="mb-4">
+                        <h3 class="section-title-common-content"><span>05</span>TERMINACIÓN DEL SERVICIO</h3>
+                        <p>Al acceder y utilizar el sitio web quam.com.co, aceptas que estás sujeto a los Términos y Condiciones, y todas las leyes aplicables regirán y se
+                            interpretarán de conformidad con las leyes Colombianas</p>
+                    </div>
+                    <div class="mb-4">
+                        <h3 class="section-title-common-content"><span>06</span>USO DEL SITIO</h3>
+                        <p>El acoso de cualquier manera o forma del sitio, incluso a través de correo electrónico y chat o por lenguaje obsceno o abusivo está
+                            estrictamente prohibido.</p>
+                        <p>La suplantación de identidad está prohibida, inclusive para los directivos o empleados con licencia, el anfitrión, o representante de QUAM, así
+                            como para otros miembros o visitantes.</p>
+                        <p>No se puede cargar, distribuir o publicar a través de este sitio cualquier contenido que sea difamatorio, calumnioso, obsceno, amenazante,
+                            invasor de la privacidad o de los derechos de publicidad, abusivo, ilegal, o de cualquier otra manera que puedan constituir o fomentar un
+                            delito penal, violar los derechos de cualquiera de las partes o que de lo contrario puede dar lugar a responsabilidad civil o violar cualquier
+                            ley.</p>
+                        <p> El contenido comercial para solicitar a otros a unirse o ser miembros de cualquier otro servicio comercial en línea u organización está
+                            prohibido.</p>
+                        <p>
+                            Para una mejor experiencia de compra en nuestra tienda online y garantizar que la información que estás viendo en el sitio está actualizada,
+                            te recomendamos usar los siguientes navegadores: Firefox:37.0 o superior, Chrome: 41.0. o superior.</p>
+                    </div>
+                    <div class="mb-4">
+                        <h3 class="section-title-common-content"><span>07</span>VÍNCULOS A TERCEROS</h3>
+                        <p>En un intento de dar más valor a nuestros visitantes, QUAM puede elegir varios sitios web de terceros para acceder desde ellos. Sin embargo,
+                            incluso si el tercero fue contratado por QUAM, no tenemos ningún control sobre estos sitios, los cuales pueden tener prácticas de privacidad
+                            y de recopilación de datos diferentes e independientes a las nuestras. QUAM no tiene ninguna responsabilidad u obligación por estas
+                            políticas o acciones de terceros, y no es responsable por las prácticas de privacidad o el contenido de dichos sitios web.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer() ?>
